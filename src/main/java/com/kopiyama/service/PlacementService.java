a07d05bcfc997fe464383f55e93976e4a28fb63c
KopiYama <mrrizkya46@gmail.com>
2024-04-14 14:07:16 +0700

    #12 Update with just one repository Membuat pembaharuan dengan menggunakan 1 repository saja, yaitu RepositoryEmployee. Lalu pada PlacementService nya mengestrak objek placment yang ada di RepositoryEmployee. Agar menjadi Placement Unik.

--- src/main/java/com/kopiyama/service/PlacementService.java
+++ src/main/java/com/kopiyama/service/PlacementService.java
@@ -1,16 +1,31 @@
 package com.kopiyama.service;
 
+import com.kopiyama.model.Employee;
 import com.kopiyama.model.Placement;
-import com.kopiyama.repository.RepositoryPlacement;
+import com.kopiyama.repository.RepositoryEmployee;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class PlacementService {
 
     public double getUmkByPlacementName(String placementName) {
-        for (Placement placement : RepositoryPlacement.getAllPlacements()) {
+        Set<Placement> uniquePlacements = extractUniquePlacements();
+        for (Placement placement : uniquePlacements) {
             if (placement.getPlacementName().equalsIgnoreCase(placementName)) {
                 return placement.getPlacementUmk();
             }
         }
         throw new IllegalArgumentException("Placement not found");
+    }
+
+    private Set<Placement> extractUniquePlacements() {
+        Set<Placement> uniquePlacements = new HashSet<>();
+        List<Employee> employees = RepositoryEmployee.getAllEmployee();
+        for (Employee emp : employees) {
+            uniquePlacements.add(emp.getPlacement());
+        }
+        return uniquePlacements;
     }
 }
